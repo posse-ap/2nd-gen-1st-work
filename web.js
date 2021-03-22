dc4cc6d0ac90a6fa2ad7873dda043f553c927a34
takanashiayane <ayane.050513@icloud.com>
2021-03-22 15:29:01 +0900

    遅れてすみません

---
+++ web.js
@@ -0,0 +1,19 @@
+function modal() {
+    var modal = document.getElementById('modal');
+    if(!modal) return;
+  
+    // var blackBg = document.getElementById('js-black-bg');
+    // var closeBtn = document.getElementById('js-close-btn');
+    // var showBtn = document.getElementById('js-show-popup');
+  
+    // closePopUp(blackBg);
+    // closePopUp(closeBtn);
+    // closePopUp(showBtn);
+    function closeModal()) {
+      if(!button) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+  }
+  popupImage();
